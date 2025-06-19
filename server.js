c69c6bc8d55cbca3b88dd02bf1172211998151b6
dhruv patel <dhruvjpatel5@gmail.com>
2025-06-19 15:54:56 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const colors = require('colors')
+const app = express()
+const PORT = process.env.PORT || 8080
+
+//middleware
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(morgan('dev'))
+
+app.listen(PORT,()=>{
+    console.log(`server running in dev port: ${process.env.DEV_MODE} on ${PORT}`)
+})
+
