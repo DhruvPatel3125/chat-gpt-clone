b20ea66a2457cde7080c3c77565a1d708021a66e
dhruv patel <dhruvjpatel5@gmail.com>
2025-06-21 21:39:51 +0530

    we create error handling and registraton controller to our page

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,31 @@
-exports.registerController = async () =>{}; 
+const userModel = require('../models/userModel');
+const errorResponse = require('../utils/errorResponse');
+
+//jwt
+exports.sendToken = (user,statusCode,res)=>{
+    const token = user.getSignedToken
+    res.status(statusCode).json({
+        success:true,
+        token,
+    });
+};
+
+//register 
+exports.registerController = async (req,res,next) =>{
+    try {
+        const {username, email, password} = req.body
+        //existing user
+        const existingEmail = await userModel.findOne({email})
+        if(existingEmail){
+            return next(new errorResponse('Email is already register',500))
+        }
+        const user = await userModel.create({username,email,password})
+        sendToken(user,201,res)
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+}; 
 exports.loginController = async () =>{};
 exports.logoutController = async () =>{};
 
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser')
 const colors = require('colors')
 const app = express()
 const PORT = process.env.PORT || 8080
+const errorHandler = require('./middlewares/errorMiddleware');
 
 //api routs
 app.use('/api/v1/auth',authRoutes)
@@ -14,7 +15,7 @@ const connectDB = require('./config/db');
 
 //routs path
 
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
 
 
 //mongo connection
@@ -26,6 +27,7 @@ app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(morgan('dev'))
+app.use(errorHandler)
 
 app.listen(PORT,()=>{
     console.log(`server running in dev port: ${process.env.DEV_MODE} on ${PORT}`)
