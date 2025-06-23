0e8b63cbdc502b32f6dad79715c78469cb59535d
dhruv patel <dhruvjpatel5@gmail.com>
2025-06-23 18:49:28 +0530

    solve error

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, Link, Typography, useTheme } from "@mui/material";
-import { Link } from "react-router-dom";
+
 
 const Navbar = () => {
   const theme = useTheme();
