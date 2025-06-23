d428c98f846329265889b597c355bc6e1d613925
dhruv patel <dhruvjpatel5@gmail.com>
2025-06-23 18:47:55 +0530

    mern stack navbar and routing chatgpt clone mern stack porject mern app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
-import './App.css'
-
-const App = () =>{
-return(
-  <>
-  <h1>Wellcome to our world</h1>
-  
-  </>
-)
-}
-export default App
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { themeSettings } from "./Theam";
+import {createTheme} from "@mui/material/styles"
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+const App = () => {
+  const theme = useMemo(() => createTheme(themeSettings));
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </ThemeProvider>
+    </>
+  );
+};
+export default App;
